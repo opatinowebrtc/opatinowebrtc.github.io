7db53a2eb5ed97e5d67db3061002fe958efaadb5
Oscar Patiño <opatinowebrtc@gmail.com>
2015-02-26 12:16:17 +0100

    chrome compatibility

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function load() {
         return;
       }
       swr.unregister().then(function(result) {
-        dump('Client context: unregister registration ' +
+        console.log('Client context: unregister registration ' +
              (result ? 'succeed' : 'failed') + '!\n');
       });
     });
@@ -16,24 +16,24 @@ window.addEventListener('DOMContentLoaded', function load() {
     navigator.serviceWorker.register('js/service.js').then(
       function(swr) {
         if (swr.installing) {
-          dump('Client context: registration has an installing' +
+          console.log('Client context: registration has an installing' +
                ' worker (' + swr.installing.state + ')!\n');
           // Once the installing worker is set and it changes is because
           // either the waiting worker or the active one is set.
           swr.installing.addEventListener('statechange', function(evt) {
             if (swr.waiting) {
-              dump('Client context: registration has an waiting worker' +
+              console.log('Client context: registration has an waiting worker' +
                    '(' + swr.waiting.state + ')!\n');
             }
             if (swr.active) {
-              dump('Client context: registration has an active worker ' +
+              console.log('Client context: registration has an active worker ' +
                    '(' + swr.active.state + ')!\n');
             }
           });
         }
       },
       function(error) {
-        dump('Client context: ' + error + '\n');
+        console.log('Client context: ' + error + '\n');
       });
   }
 });
