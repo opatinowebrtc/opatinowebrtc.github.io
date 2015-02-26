bb53f259fe0a44856e97849d7d95ba883468f1e5
Oscar Patiño <opatinowebrtc@gmail.com>
2015-02-26 16:01:47 +0100

    `date`

--- js/app.js
+++ js/app.js
@@ -1,26 +1,51 @@
 'use strict';
+
 window.addEventListener('DOMContentLoaded', function load() {
   window.removeEventListener('DOMContentLoaded', load);
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistration().then(function(swr) {
-      if (!swr) {
+    document.getElementById('check-controller').onclick = function() {
+      if (!navigator.serviceWorker.controller) {
+        dump('Client context: no navigator.serviceWorker.controller object!\n');
         return;
       }
-      swr.unregister().then(function(result) {
-        dump('Client context: unregister registration ' +
-             (result ? 'succeed' : 'failed') + '!\n');
+      dump('Client context: navigator.serviceWorker.controller.state is ' +
+           (navigator.serviceWorker.controller.state)+ '\n');
+      navigator.serviceWorker.getRegistration().then(swr => {
+        if (!swr) {
+          return;
+        }
+        if (swr.installed) {
+          dump('Client context (register): registration has an installed' +
+               'worker (' + swr.installed.state + ')!\n');
+        }
       });
-    });
-    
+    };
+
+    document.getElementById('unregister-registration').onclick = function() {
+      navigator.serviceWorker.getRegistration().then(swr => {
+        if (!swr) {
+          return;
+        }
+        swr.unregister().then(result => {
+          dump('Client context: unregister registration ' +
+               (result ? 'succeed' : 'failed') + '!\n');
+        });
+      });
+    };
+
+    if (navigator.serviceWorker.controller) {
+      return;
+    }
+
     navigator.serviceWorker.register('js/service.js').then(
-      function(swr) {
+      swr => {
         if (swr.installing) {
           dump('Client context: registration has an installing' +
                ' worker (' + swr.installing.state + ')!\n');
           // Once the installing worker is set and it changes is because
           // either the waiting worker or the active one is set.
-          swr.installing.addEventListener('statechange', function(evt) {
+          swr.installing.addEventListener('statechange', evt => {
             if (swr.waiting) {
               dump('Client context: registration has an waiting worker' +
                    '(' + swr.waiting.state + ')!\n');
@@ -32,8 +57,8 @@ window.addEventListener('DOMContentLoaded', function load() {
           });
         }
       },
-      function(error) {
-        dump('Client context: ' , error);
+      error => {
+        dump('Client context: ' + error + '\n');
       });
   }
 });
