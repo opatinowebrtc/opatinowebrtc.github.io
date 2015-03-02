abfc5905edf5ce16ab02fb88f00d75a2aec1137c
Oscar Patiño <opatinowebrtc@gmail.com>
2015-03-02 16:48:04 +0100

    test caches.match

--- js/service.js
+++ js/service.js
@@ -106,6 +106,7 @@ self.addEventListener('fetch', evt => {
   }
 
   evt.respondWith(
+<<<<<<< HEAD
     caches.open('v1').then(cache => {
       debug('cache created or recovered ' + cache);
       caches.open('v2').then(result => {
@@ -122,6 +123,42 @@ self.addEventListener('fetch', evt => {
             });
           });
         });
+=======
+    caches.open('v1').then(function(cache) {
+      return cache.matchAll('/').then(response => {
+        debug(JSON.stringify(response));
+        // response.map(function(r){
+        //   debug('MatchAll responded cache match ' + r);
+        //   return r;
+      // return cache.match(request).then(function(response) {
+      //   if (response) {
+      //     url = new URL(response.url);
+      //     debug('found response in cache: ' + url.pathname);
+      //     // if(url.pathname === '/img/mozilla2.png') {
+      //     //   cache.delete('/img/mozilla2.png').then(response => {
+      //     //   debug('delete works, deleted mozilla2.png');
+      //     // });
+      //     debug('testing keys...');
+      //     //cache.keys('/').then(reponse => {
+      //     //  debug('keys response: ' + JSON.stringify(reponse));
+      //     // });
+      //     // return response;
+      //   } else {
+      //     var reqURL = new URL(request.url);
+      //     debug('no response found in cache. Fetching from network ' + reqURL);
+
+      //       // fetch(request).then(response => {
+      //       //   var response2 = response;
+      //       //   cache.put(request, response2).then( response3 => {
+      //       //     debug('cache put works, put mozilla2.png in cache');
+      //       //   });
+      //       // });
+
+      //     // return fetch(request);
+      //   }
+      }, function(error) {
+        debug('error in cache.match ' + error);
+>>>>>>> parent of 4f15444... testing matchall
       });
     })
   );
