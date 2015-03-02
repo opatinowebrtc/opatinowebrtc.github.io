4cc61260614b929db768a8d5e2922ce3f14b7ab1
Oscar Patiño <opatinowebrtc@gmail.com>
2015-03-02 10:37:41 +0100

    testing cacheStorage delete

--- js/service.js
+++ js/service.js
@@ -48,6 +48,55 @@ self.addEventListener('activate', evt => {
   evt.waitUntil(delaysAsActivated());
 });
 
+// self.addEventListener('fetch', evt => {
+//   var request = evt.request;
+//   var url = new URL(request.url);
+  
+//   if (DEBUG) {
+//     debug('fetching ' + url.pathname);
+//   }
+
+//   evt.respondWith(
+//     caches.open('v1').then(function(cache) {
+//       return cache.matchAll(request).then(response => {
+//         debug(JSON.stringify(response));
+//         response.map(function(r){
+//           debug('MatchAll responded cache match ' + r);
+//           return r;
+//       return cache.match(request).then(function(response) {
+//         if (response) {
+//           url = new URL(response.url);
+//           debug('found response in cache: ' + url.pathname);
+//           if(url.pathname === '/img/mozilla2.png') {
+//             cache.delete('/img/mozilla2.png').then(response => {
+//             debug('delete works, deleted mozilla2.png');
+//           });
+//           debug('testing keys...');
+//           cache.keys('/').then(reponse => {
+//            debug('keys response: ' + JSON.stringify(reponse));
+//           });
+//           return response;
+//         } else {
+//           var reqURL = new URL(request.url);
+//           debug('no response found in cache. Fetching from network ' + reqURL);
+
+//             fetch(request).then(response => {
+//               var response2 = response;
+//               cache.put(request, response2).then( response3 => {
+//                 debug('cache put works, put mozilla2.png in cache');
+//               });
+//             });
+
+//           // return fetch(request);
+//         }
+//       }, function(error) {
+//         debug('error in cache.match ' + error);
+//       });
+//     }, function(error) { debug('error in caches.open ' + error); })
+//   );
+// });
+
+
 self.addEventListener('fetch', evt => {
   var request = evt.request;
   var url = new URL(request.url);
@@ -57,44 +106,16 @@ self.addEventListener('fetch', evt => {
   }
 
   evt.respondWith(
-    caches.open('v1').then(function(cache) {
-      return cache.matchAll(request).then(response => {
-        debug(JSON.stringify(response));
-        // response.map(function(r){
-        //   debug('MatchAll responded cache match ' + r);
-        //   return r;
-      // return cache.match(request).then(function(response) {
-      //   if (response) {
-      //     url = new URL(response.url);
-      //     debug('found response in cache: ' + url.pathname);
-      //     // if(url.pathname === '/img/mozilla2.png') {
-      //     //   cache.delete('/img/mozilla2.png').then(response => {
-      //     //   debug('delete works, deleted mozilla2.png');
-      //     // });
-      //     debug('testing keys...');
-      //     //cache.keys('/').then(reponse => {
-      //     //  debug('keys response: ' + JSON.stringify(reponse));
-      //     // });
-      //     // return response;
-      //   } else {
-      //     var reqURL = new URL(request.url);
-      //     debug('no response found in cache. Fetching from network ' + reqURL);
-
-      //       // fetch(request).then(response => {
-      //       //   var response2 = response;
-      //       //   cache.put(request, response2).then( response3 => {
-      //       //     debug('cache put works, put mozilla2.png in cache');
-      //       //   });
-      //       // });
-
-      //     // return fetch(request);
-      //   }
-      }, function(error) {
-        debug('error in cache.match ' + error);
-      });
-    }, function(error) { debug('error in caches.open ' + error); })
-  );
-});
+    caches.open('v1').then(cache => {
+      debug('cache created or recovered ' + cache);
+      return cache;
+    });
+    caches.delete('v1').then(result => {
+      debug('cache v1 successfully deleted');
+      debug('deleted result = ' + result);
+      return result;
+    });
+);
 
 self.addEventListener('message', evt => {
   caches.open('v1').then( cache => {
