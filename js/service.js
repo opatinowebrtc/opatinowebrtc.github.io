712d5fc3be08923838d6788451cbafe5be765e1f
Oscar Patiño <opatinowebrtc@gmail.com>
2015-03-09 10:42:08 +0100

    matchall fixed

--- js/service.js
+++ js/service.js
@@ -191,8 +191,65 @@
 
 
 
+// 'use strict';
+// var DEBUG = true;
+// if (!self.debug) {
+//   self.debug = function debug(message) {
+//     dump("Execution context: " + message + "\n");
+//   };
+// }
+
+// self.addEventListener('install', evt => {
+//   if (DEBUG) {
+//     debug('install event fired!');
+//   }
+
+//   function delaysAsInstalled() {
+//     return Promise.resolve();
+//   }
+//   evt.waitUntil(
+//     caches.open('v1').then(function(cache) {
+//       debug('caching image into cache');
+//       return cache.addAll([
+//         '/',
+//         '/css/app.css',
+//         '/js/app.js',
+//         '/img/mozilla.png']);
+//     });
+//   );
+// });
+
+// self.addEventListener('activate', evt => {
+//   if (DEBUG) {
+//     debug('activate event fired!');
+//   }
+//   function delaysAsActivated() {
+//     return Promise.resolve();
+//   }
+//   evt.waitUntil(delaysAsActivated());
+// });
+
+// self.addEventListener('fetch', evt => {
+//   var request = evt.request;
+//   var url = new URL(request.url);
+//   if (DEBUG) {
+//     debug('fetching ' + url.pathname);
+//   }
+//   evt.respondWith(
+//     caches.open('v1').then(function(cache) {
+//       return cache.matchAll().then(response => {
+//         response.map(resp => {
+//           debug(resp.url);
+//         });
+//       });
+//     });
+//   );
+// });
+
 'use strict';
+
 var DEBUG = true;
+
 if (!self.debug) {
   self.debug = function debug(message) {
     dump("Execution context: " + message + "\n");
@@ -207,16 +264,7 @@ self.addEventListener('install', evt => {
   function delaysAsInstalled() {
     return Promise.resolve();
   }
-  evt.waitUntil(
-    caches.open('v1').then(function(cache) {
-      debug('caching image into cache');
-      return cache.addAll([
-        '/',
-        '/css/app.css',
-        '/js/app.js',
-        '/img/mozilla.png']);
-    });
-  );
+  evt.waitUntil(delaysAsInstalled());
 });
 
 self.addEventListener('activate', evt => {
@@ -232,16 +280,10 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   var request = evt.request;
   var url = new URL(request.url);
+  
   if (DEBUG) {
     debug('fetching ' + url.pathname);
   }
-  evt.respondWith(
-    caches.open('v1').then(function(cache) {
-      return cache.matchAll().then(response => {
-        response.map(resp => {
-          debug(resp.url);
-        });
-      });
-    });
-  );
+
+  evt.respondWith(fetch(request));
 });
